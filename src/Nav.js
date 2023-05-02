2c84abf86464a80de66c837708a39d406c8876b0
Tushar Joshi <beingtushar2001@gmail.com>
2023-05-02 22:37:01 +0530

    first

---
+++ src/Nav.js
@@ -0,0 +1,38 @@
+import React, {useEffect, useState} from 'react'
+import "./Nav.css";
+function Nav() {
+
+    const [show,handleShow] = useState(false);
+    const transitionNavbar = () => {
+        if(window.scrollY>100){       
+            handleShow(true);
+        } else {
+            handleShow(false);
+        }
+    }
+
+    useEffect( () => {
+        window.addEventListener("scroll",transitionNavbar);
+        return() => {
+            window.removeEventListener("scroll",transitionNavbar);
+        }
+    }, []);
+
+    return (
+        <div className={`nav ${show && "nav_black"}`}>
+            <div className="nav_contents">
+                <img className="nav_logo"
+                    src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+                    alt=""
+                />
+
+                <img className="nav_avatar"
+                    src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+                    alt=""
+                />
+            </div>
+        </div>  
+    )
+}
+
+export default Nav
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAzf_JWAcYiPSKVfnam1wqDImsewtuvz-Y",
+    authDomain: "netflix-clone-e1d86.firebaseapp.com",
+    projectId: "netflix-clone-e1d86",
+    storageBucket: "netflix-clone-e1d86.appspot.com",
+    messagingSenderId: "374771325628",
+    appId: "1:374771325628:web:f4ed9a5d9fe16188284eb9"
+  };
